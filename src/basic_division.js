6d0fd645ad05bf7b00ec16c801fad36e7ad15da5
Mariko Hisakawa <avenafatua@gmail.com>
2021-02-15 14:51:21 +0900

    Customization of division remainders

--- src/basic_division.js
+++ src/basic_division.js
@@ -1,22 +1,16 @@
 class BasicDivision {
   constructor () {
   }
-  _generatePage (denominator, result, remainder, maxRemainder, problemsPerPage, option) {
+  _generatePage (denominator, result, remainder, problemsPerPage, option) {
     let problems = [];
     for (let problemIndex=0; problemIndex<problemsPerPage ; problemIndex++) {
       const denominatorValue = denominator.pick();
       const resultValue = result.pick();
       let remainderValue = 0;
       if (remainder && denominatorValue > 1) {
-        if (maxRemainder > 0) {
-          remainderValue = Math.ceil(Math.random() * Math.min(denominatorValue - 1, maxRemainder));
-        } else {
-          // Remainder range not specified
-          remainderValue = Math.ceil(Math.random() * (denominatorValue - 1));
-        }
-        // TODO remove it
-        remainderValue = 2;
-        
+        const minRemainder = remainder.from;
+        const maxRemainder = Math.min(remainder.to, denominatorValue-1);
+        remainderValue = minRemainder + Math.floor((maxRemainder - minRemainder + 1) * Math.random());
       }
       const numeratorValue = denominatorValue * resultValue + remainderValue;
       let items = [];
@@ -26,7 +20,7 @@ class BasicDivision {
       items.push(new ItemNumber(denominatorValue));
       items.push(new ItemOperand('='));
       items.push(new ItemBox(resultValue, !option.box));
-      if (remainderValue > 0) {
+      if (remainderValue > 0 && option.box) {
         items.push(new ItemOperand('...'));
         items.push(new ItemBox(remainderValue, !option.box));
       }
@@ -35,18 +29,20 @@ class BasicDivision {
     return problems;
   }
   generate (pages, problemsPerPage, option) {
-    if (option.remainder && option.denominator.from <= 1) {
-      option.denominator.from = 2;
-    }
-    if (option.remainder && option.denominator.to <= 1) {
-      option.denominator.to = 2;
+    if (option.remainder) {
+      if (option.denominator.from <= option.remainder.from) {
+        option.denominator.from = option.remainder.from + 1;
+      }
+      if (option.denominator.to <= option.remainder.from) {
+        option.denominator.to = option.remainder.from + 1;
+      }
     }
     let result = new WeighedRandom(option.result, (option.result.resetRandom)? problemsPerPage:problemsPerPage*pages);
     let suite = [];
     if (option.randomizeDenominator) {
       let denominator = new WeighedRandom(option.denominator);
       for (let pageIndex=0; pageIndex<pages; pageIndex++) {
-        const pageContent = this._generatePage(denominator, result, option.remainder, option.maxRemainder, problemsPerPage, option);
+        const pageContent = this._generatePage(denominator, result, option.remainder, problemsPerPage, option);
         suite.push(pageContent);
         if (option.denominator.resetRandom) {
           denominator.reset();
@@ -57,7 +53,7 @@ class BasicDivision {
       for (let pageIndex=0; pageIndex<pages; pageIndex++) {
         let denominatorVal = denominatorRand.pick();
         let denominator = new WeighedRandom({from:denominatorVal, to:denominatorVal});
-        const pageContent = this._generatePage(denominator, result, option.remainder, option.maxRemainder, problemsPerPage, option);
+        const pageContent = this._generatePage(denominator, result, option.remainder, problemsPerPage, option);
         suite.push(pageContent);
         if (option.denominator.resetRandom) {
           denominator.reset();
